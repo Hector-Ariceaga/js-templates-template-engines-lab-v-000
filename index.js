ce1a6be4ad7da4b116e25355bd9d9af81be628a9
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-19 22:35:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function createPost() {
   console.log(postTemplate)
   console.log(commentsTemplate)
 
-  pageTemplate() += document.getElementByTagName('main').innerHTML
+  document.getElementByTagName('main').innerHTML += pageTemplate()
 
   let blog = postTemplate({'postTitle': postTitle, 'postAuthor': postAuthor, 'postText': postText});
   let comments = commentsTemplate();
