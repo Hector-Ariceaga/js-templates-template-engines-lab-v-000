1df08882463b8664f4183b6e14c0479f0a3ab5ee
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-19 22:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ function createPost() {
   let postTitle = document.getElementById('titleName').value
   let postAuthor = document.getElementById('authorName').value
   let postText = document.getElementById('postText').value
-  console.log(postTitle)
-  console.log(postAuthor)
-  console.log(postText)
+
+  let pageTemplate = _.template(document.getElementById('page-template').innerHTML)
+  let postTemplate = _.template(document.getElementById('post-template').innerHTML)
+  let commentsTemplate = _.template(document.getElementById('comments-template').innerHTML)
+  
+  console.log(pageTemplate)
+  console.log(postTemplate)
+  console.log(commentsTemplate)
 
   let postTemplate = document.getElementById('post-template').innerHTML;
 
